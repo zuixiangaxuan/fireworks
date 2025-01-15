5a870aec3a14007a1c95fbab89ee3955cb08dcba
zuixiangaxuan <1657829886@QQ.COM>
2025-01-15 23:00:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||莉|莉|我|喜|欢|你|#rectangle|');
     }
 
     S.Drawing.loop(function () {
